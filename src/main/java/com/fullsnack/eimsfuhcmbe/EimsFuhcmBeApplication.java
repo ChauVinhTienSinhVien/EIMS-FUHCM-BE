2db1e46fe25904143c0c78a438a5c8c875b0716f
= <tiencvse183243@fpt.edu.vn>
2024-09-23 21:50:49 +0700

    set up srping boot starter project

---
+++ src/main/java/com/fullsnack/eimsfuhcmbe/EimsFuhcmBeApplication.java
@@ -0,0 +1,13 @@
+package com.fullsnack.eimsfuhcmbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EimsFuhcmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EimsFuhcmBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullsnack/eimsfuhcmbe/EimsFuhcmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullsnack.eimsfuhcmbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EimsFuhcmBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

994fab391e4afad90b480cc4f8760e4e3192f39d
= <tiencvse183243@fpt.edu.vn>
2024-10-21 09:19:37 +0700

    new db

--- src/main/java/com/fullsnacke/eimsfuhcmbe/service/authentication/AuthenticationService.java
+++ src/main/java/com/fullsnacke/eimsfuhcmbe/service/authentication/AuthenticationService.java
@@ -136,7 +136,7 @@ public class AuthenticationService {
 
         String currentUserEmail = userOptional.map(User::getEmail).orElse(null);
         User user = userRepository.findByEmail(currentUserEmail).orElse(null);
-
+        //User user = userRepository.findByEmail(requestBody.getEmail()).orElse(null);
         if (user == null) {
             throw new EntityNotFoundException(User.class, "email", requestBody.getEmail());
         }

6fdd5d3535d6bb4517d050f3fbe7468ca33ffc89
= <tiencvse183243@fpt.edu.vn>
2024-10-23 22:15:53 +0700

    update getUserInvigilatorAttendanceBySemesterIdAndApproved

--- src/main/java/com/fullsnacke/eimsfuhcmbe/service/InvigilatorAttendanceServiceImpl.java
+++ src/main/java/com/fullsnacke/eimsfuhcmbe/service/InvigilatorAttendanceServiceImpl.java
@@ -354,6 +354,10 @@ public class InvigilatorAttendanceServiceImpl implements InvigilatorAttendanceSe
         return invigilatorAttendanceRepository.findInvigilatorAttendanceByInvigilatorIdAndDay(currentUser.get().getId(), day);
     }
 
+    public List<InvigilatorAttendance> getUserInvigilatorAttendanceBySemesterIdAndApproved(Integer invigilatorId, Integer semesterId) {
+        return invigilatorAttendanceRepository.findInvigilatorAttendanceByInvigilatorIdAndSemesterIdAndApprove(invigilatorId, semesterId);
+    }
+
     public List<InvigilatorAttendance> getCurrentUserInvigilatorAttendanceBySemesterIdAndApproved(Integer semesterId) {
         Optional<User> currentUser = SecurityUntil.getLoggedInUser();
         if(currentUser.isEmpty()){

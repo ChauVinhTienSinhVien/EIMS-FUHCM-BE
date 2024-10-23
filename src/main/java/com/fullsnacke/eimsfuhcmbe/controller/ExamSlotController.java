c83aae3d774589d518b90a3d066fc796bba9ffbb
thien-lm30 <thienlmse183143@fpt.edu.vn>
2024-10-23 21:23:34 +0700

    fix get today examslot

--- src/main/java/com/fullsnacke/eimsfuhcmbe/controller/ExamSlotController.java
+++ src/main/java/com/fullsnacke/eimsfuhcmbe/controller/ExamSlotController.java
@@ -245,7 +245,13 @@ public class ExamSlotController {
     public ResponseEntity<List<ExamSlotResponseDTO>> getExamSlotsForToday() {
         ZonedDateTime startTime = ZonedDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
         ZonedDateTime endTime = ZonedDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999999999);
-        List<ExamSlot> examSlotList = examSlotServiceImpl.getExamSlotsInTimeRange(startTime, endTime);
+        List<ExamSlot> examSlotListInTime = examSlotServiceImpl.getExamSlotsInTimeRange(startTime, endTime);
+        List<ExamSlot> examSlotList = new ArrayList<>();
+        for (ExamSlot e:examSlotListInTime) {
+            if (e.getStatus() == ExamSlotStatus.APPROVED.getValue()) {
+                examSlotList.add(e);
+            }
+        }
         if (examSlotList.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
